765200aa22b98115412680c1a15fc0185227ed2b
Dana <danaferh95@alumnos.cei.es>
2024-05-08 12:54:54 +0200

    Se agrega el middleware de PUT. Aqui va con la solucion de Joaquin, corregir mi solucion la parte de enviar error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const {json} = require("body-parser");
+const cors = require("cors");
 const {tareas, crearTarea, borrarTarea, toggleEstado, editarTexto} = require("./db");
 
 
@@ -54,16 +55,65 @@ servidor.delete("/tareas/borrar/:id([0-9]+)", async (peticion,respuesta)=>{
     
 });
 
+servidor.put("/tareas/actualizar/:id([0-9]+)/:operacion(1|2)", async (peticion,respuesta,siguiente) => {
+    let operacion = Number(peticion.params.operacion);
+    let funciones = [editarTexto,toggleEstado];
 
-servidor.use((peticion,respuesta) => {
-    respuesta.status(404);
-    respuesta.json({ error : "recurso no encontrado" });
+    if(operacion == 1 && (!peticion.body.tarea || peticion.body.tarea.trim() == "")){
+        return siguiente(true);
+    }
+
+    try{
+        let count = await funciones[operacion - 1](peticion.params.id, operacion == 1 ? peticion.body.tarea : null);
+
+        respuesta.json({ resultado : count ? "ok" : "ko" });
+
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error);
+    }
+
 });
+
+/* MI SOLUCION FALTA EL ENVIO DEL ERROR
+servidor.put("/tareas/actualizar/:id([0-9]+)/:operacion([1-2])",  async (peticion,respuesta)=>{
+
+    if(peticion.params.operacion == 1 && (!peticion.body.tarea || peticion.body.tarea.trim() == "")){
+        return siguiente(true);
+   }
+
+    try{
+        if(peticion.params.operacion == 2){
+            let count = await toggleEstado(peticion.params.id);
+            respuesta.json({resultado : count ? "ok" : "ko"});
+        }else{
+                       
+            let count = await editarTexto(peticion.params.id, peticion.body.tarea);
+            respuesta.json({resultado : count ? "ok" : "ko"});
+           
+        }
+
+    }catch(error){
+            respuesta.status(500);
+            respuesta.json(error);
+    }
+    
+   //respuesta.send(` el id: ${peticion.params.id} y la operación sera ${peticion.params.operacion}`);
+    
+}); */
+
 
 servidor.use((error,peticion,respuesta,siguiente) => {
     
     respuesta.status(400);
     respuesta.json({ error : "error en la petición"});
+});
+
+
+servidor.use((peticion,respuesta) => {
+    respuesta.status(404);
+    respuesta.json({ error : "recurso no encontrado" });
 });
 
 
